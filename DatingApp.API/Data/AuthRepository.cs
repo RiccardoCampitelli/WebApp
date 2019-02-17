ceff2f3d47d7e1c05b9e719f5793dcabe18dadef
Ayahuascax <riccardo.campitelli91@gmail.com>
2019-02-17 18:24:02 +0000

    Added Authorization and tokens

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.Data
         private readonly DataContext _context;
         public AuthRepository(DataContext context)
         {
-            this._context=context;
+            _context=context;
         }
         public async Task<User> Login(string username, string password)
         {
@@ -19,10 +19,26 @@ namespace DatingApp.API.Data
             if(user==null){
                 return null;
             }
-            //if(!VerifyPasswordHash)
+            if(!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            return null;
+
             return user;
         }
 
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using( var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)){
+               
+               var computedHash=hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+               for(int i = 0; i<computedHash.Length;i++){
+                   if(computedHash[i]!=passwordHash[i]){
+                       return false;
+                   }
+
+               }
+           }
+           return true;
+        }
 
         public async Task<User> Register(User user, string password)
         {
@@ -45,9 +61,12 @@ namespace DatingApp.API.Data
            }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if(await _context.Users.AnyAsync(x=>x.Username==username))
+                return true;
+            
+            return false;
         }
     }
 }
